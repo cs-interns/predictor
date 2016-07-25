7d99fb0dccbc7f7d5eaa95e3accdee31c56a75b7
rjesteban <ajcp.esteban@gmail.com>
2016-07-25 14:11:01 +0800

    Add Chart JS to display graphs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require materialize-sprockets
 //= require turbolinks
+//= require Chart
 //= require upload_file.coffee
 //= require predictions.coffee
 //= require static_pages.coffee
